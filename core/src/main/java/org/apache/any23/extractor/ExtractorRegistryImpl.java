3313e4889367b13ddde1168e3bd1bdbd58276685
Peter Ansell <p_ansell@yahoo.com>
2012-11-27 14:42:37 +1000

    GeoExtractorFactory

--- core/src/main/java/org/apache/any23/extractor/ExtractorRegistryImpl.java
+++ core/src/main/java/org/apache/any23/extractor/ExtractorRegistryImpl.java
@@ -89,8 +89,8 @@ public class ExtractorRegistryImpl extends info.aduna.lang.service.ServiceRegist
                 instance.register(TitleExtractor.factory);
                 instance.register(XFNExtractor.factory);
                 instance.register(ICBMExtractor.factory);
-                instance.register(AdrExtractor.factory);
-                instance.register(GeoExtractor.factory);
+                //instance.register(AdrExtractor.factory);
+                //instance.register(GeoExtractor.factory);
                 instance.register(HCalendarExtractor.factory);
                 instance.register(HCardExtractor.factory);
                 instance.register(HListingExtractor.factory);
